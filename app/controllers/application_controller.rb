b5104af6071347de6e408a7c91e11896b7e1f891
Chandan Kumar <chandan.jhun@gmail.com>
2013-09-08 21:01:22 +0530

    form_tag for session creation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,5 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   include SessionsHelper
+  #http_basic_authenticate_with :name => 'chandan', :password => 'secret'
 end
